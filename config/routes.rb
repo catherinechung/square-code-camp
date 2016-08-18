c9dde482917c4953cc65a334f0784f81cbdca391
Devanshi Vyas <dnv2103@columbia.edu>
2016-08-18 13:35:15 -0400

    dropdown changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'library/index'
-
   resources :ideas
+  get 'library/index'
+  root 'ideas#index'
+
+  #resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816182406) do
+ActiveRecord::Schema.define(version: 20160818035656) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20160816182406) do
     t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
