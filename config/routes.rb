0cbd8dead4106ad80bdbb518be2c0aa3b97ac07d
Catherine Chung <cathercc@usc.edu>
2016-08-18 10:28:09 -0400

    routed home to library, added footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'library/index'
 
   resources :ideas
+  resources :library
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
